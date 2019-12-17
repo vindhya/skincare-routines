1658330797f522cfa65d0b9fdae6eb51118676e1
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-12-16 20:39:12 -0500

    Removes unnecessary files

--- apps/skincare-test-app/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'skincare-test-app',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/skincare-test-app',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
